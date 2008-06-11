f06dbda692ee873c94aa08768cc6019bb26c285a
Zack <Zack@arcemu.org>
2008-06-11 20:23:40 +0000

    speedcheat detector is disabled while on boats and transporters

--- src/ascent-world/MovementHandler.cpp
+++ src/ascent-world/MovementHandler.cpp
@@ -278,7 +278,7 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
 	/* Update player movement state                                         */
 	/************************************************************************/
 	//update the detector
-	if( sWorld.antihack_speed && !_player->GetTaxiState() )
+	if( sWorld.antihack_speed && !_player->GetTaxiState() && _player->m_TransporterGUID == 0 )
 	{
 		// simplified: just take the fastest speed. less chance of fuckups too
 		float speed = ( _player->flying_aura ) ? _player->m_flySpeed : ( _player->m_swimSpeed >_player-> m_runSpeed ) ? _player->m_swimSpeed : _player->m_runSpeed;
