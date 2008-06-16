e82eedfd43ea8598b9e374abf520fb24103f081d
vojtac <vojtac@arcemu.org>
2008-06-16 19:04:16 +0000

    * add table 'creature_timed_emotes' to world_structure.sql

--- sql/world_structure.sql
+++ sql/world_structure.sql
@@ -463,6 +463,34 @@ CREATE TABLE `creature_staticspawns` (
 
 
 
+DROP TABLE IF EXISTS `creature_timed_emotes`;
+
+#
+# Table structure for table 'creature_timed_emotes'
+#
+
+CREATE TABLE IF NOT EXISTS `creature_timed_emotes` (
+  `spawnid` int(10) unsigned NOT NULL default '0',
+  `rowid` int(10) unsigned NOT NULL default '0',
+  `type` tinyint(1) unsigned NOT NULL default '1',
+  `value` int(10) unsigned NOT NULL default '0',
+  `msg` text,
+  `msg_type` int(10) unsigned NOT NULL default '0',
+  `msg_lang` int(10) unsigned NOT NULL default '0',
+  `expire_after` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`spawnid`,`rowid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+#
+# Dumping data for table 'creature_timed_emotes'
+#
+
+# (No data found.)
+
+
+
 DROP TABLE IF EXISTS `creature_waypoints`;
 
 #
