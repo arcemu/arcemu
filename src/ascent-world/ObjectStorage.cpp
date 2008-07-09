c656e7013fe364eb3d09f69013e1ef73d6716d0f
Zack <Zack@arcemu.org>
2008-07-09 10:20:38 +0000

    http://arcemu.org/forums/index.php?showtopic=1053&hl= -> warlock grimmoire fixes

--- src/ascent-world/ObjectStorage.cpp
+++ src/ascent-world/ObjectStorage.cpp
@@ -343,96 +343,107 @@ void ObjectMgr::LoadExtraItemStuff()
 		// forced pet entries
 		switch( pItemPrototype->ItemId )
 		{
-		case 28071:
-		case 28072:
-		case 28073:
-		case 25469:
-		case 23734:
-		case 23735:
-		case 23755:
-		case 16381:
-		case 16382:
-		case 16383:
-		case 22188:
-		case 22189:
-		case 25900:
-		case 23711:
-		case 23730:
-		case 23731:
-		case 16390:
-		case 16388:
-		case 16389:
-		case 16384:
-		case 16385:
-		case 16386:
-		case 16387:
-		case 22190:
+		case 28071: //Grimoire of Anguish (Rank 1)
+		case 28072: //Grimoire of Anguish (Rank 2)
+		case 28073: //Grimoire of Anguish (Rank 3)
+		case 25469: //Grimoire of Avoidance
+		case 23734: //Grimoire of Cleave (Rank 1)
+		case 23745: //Grimoire of Cleave (Rank 2)
+		case 23755: //Grimoire of Cleave (Rank 3)
+		case 25900: //Grimoire of Demonic Frenzy
+		case 23711: //Grimoire of Intercept (Rank 1)
+		case 23730: //Grimoire of Intercept (Rank 2)
+		case 23731: //Grimoire of Intercept (Rank 3)
 			// Felguard
 			pItemPrototype->ForcedPetId = 17252;
 			break;
 
-		case 16321:
-		case 16322:
-		case 16323:
-		case 16324:
-		case 16325:
-		case 16326:
-		case 16327:
-		case 16328:
-		case 16329:
-		case 16330:
-		case 22181:
-		case 16302:
-		case 16316:
-		case 16317:
-		case 16318:
-		case 16319:
-		case 16320:
-		case 22179:
-		case 16331:
+		case 16321: //Grimoire of Blood Pact (Rank 1)
+		case 16322: //Grimoire of Blood Pact (Rank 2)
+		case 16323: //Grimoire of Blood Pact (Rank 3)
+		case 16324: //Grimoire of Blood Pact (Rank 4)
+		case 16325: //Grimoire of Blood Pact (Rank 5)
+		case 22180: //Grimoire of Blood Pact (Rank 6)
+		case 16326: //Grimoire of Fire Shield (Rank 1)
+		case 16327: //Grimoire of Fire Shield (Rank 2)
+		case 16328: //Grimoire of Fire Shield (Rank 3)
+		case 16329: //Grimoire of Fire Shield (Rank 4)
+		case 16330: //Grimoire of Fire Shield (Rank 5)
+		case 22181: //Grimoire of Fire Shield (Rank 6)
+		case 16302: //Grimoire of Firebolt (Rank 2)
+		case 16316: //Grimoire of Firebolt (Rank 3)
+		case 16317: //Grimoire of Firebolt (Rank 4)
+		case 16318: //Grimoire of Firebolt (Rank 5)
+		case 16319: //Grimoire of Firebolt (Rank 6)
+		case 16320: //Grimoire of Firebolt (Rank 7)
+		case 22179: //Grimoire of Firebolt (Rank 8)
+		case 16331: //Grimoire of Phase Shift
 			// Imp
 			pItemPrototype->ForcedPetId = 416;
 			break;
 
-		case 16357:
-		case 16358:
-		case 16359:
-		case 16360:
-		case 16361:
-		case 16362:
-		case 22184:
-		case 16351:
-		case 16352:
-		case 16353:
-		case 16354:
-		case 16355:
-		case 16356:
-		case 22185:
-		case 16346:
-		case 16347:
-		case 16348:
-		case 16349:
-		case 16350:
-		case 22182:
+		case 16357: //Grimoire of Consume Shadows (Rank 1)
+		case 16358: //Grimoire of Consume Shadows (Rank 2)
+		case 16359: //Grimoire of Consume Shadows (Rank 3)
+		case 16360: //Grimoire of Consume Shadows (Rank 4)
+		case 16361: //Grimoire of Consume Shadows (Rank 5)
+		case 16362: //Grimoire of Consume Shadows (Rank 6)
+		case 22184: //Grimoire of Consume Shadows (Rank 7)
+		case 16351: //Grimoire of Sacrifice (Rank 1)
+		case 16352: //Grimoire of Sacrifice (Rank 2)
+		case 16353: //Grimoire of Sacrifice (Rank 3)
+		case 16354: //Grimoire of Sacrifice (Rank 4)
+		case 16355: //Grimoire of Sacrifice (Rank 5)
+		case 16356: //Grimoire of Sacrifice (Rank 6)
+		case 22185: //Grimoire of Sacrifice (Rank 7)
+		case 16363: //Grimoire of Suffering (Rank 1)
+		case 16364: //Grimoire of Suffering (Rank 2)
+		case 16365: //Grimoire of Suffering (Rank 3)
+		case 16366: //Grimoire of Suffering (Rank 4)
+		case 22183: //Grimoire of Suffering (Rank 5)
+		case 28068: //Grimoire of Suffering (Rank 6)
+		case 16346: //Grimoire of Torment (Rank 2)
+		case 16347: //Grimoire of Torment (Rank 3)
+		case 16348: //Grimoire of Torment (Rank 4)
+		case 16349: //Grimoire of Torment (Rank 5)
+		case 16350: //Grimoire of Torment (Rank 6)
+		case 22182: //Grimoire of Torment (Rank 7)
 			// Voidwalker
 			pItemPrototype->ForcedPetId = 1860;
 			break;
 
-		case 16380:
-		case 16379:
-		case 16375:
-		case 16376:
-		case 16377:
-		case 16378:
-		case 22187:
-		case 16363:
-		case 16364:
-		case 16365:
-		case 16366:
-		case 22183:
-		case 28068:
+		case 16368: //Grimoire of Lash of Pain (Rank 2)
+		case 16371: //Grimoire of Lash of Pain (Rank 3)
+		case 16372: //Grimoire of Lash of Pain (Rank 4)
+		case 16373: //Grimoire of Lash of Pain (Rank 5)
+		case 16374: //Grimoire of Lash of Pain (Rank 6)
+		case 22186: //Grimoire of Lash of Pain (Rank 7)
+		case 16380: //Grimoire of Lesser Invisibility
+		case 16379: //Grimoire of Seduction
+		case 16375: //Grimoire of Soothing Kiss (Rank 1)
+		case 16376: //Grimoire of Soothing Kiss (Rank 2)
+		case 16377: //Grimoire of Soothing Kiss (Rank 3)
+		case 16378: //Grimoire of Soothing Kiss (Rank 4)
+		case 22187: //Grimoire of Soothing Kiss (Rank 5)
 			// Succubus
 			pItemPrototype->ForcedPetId = 1863;
+			break;
+
+		case 16381: //Grimoire of Devour Magic (Rank 2)
+		case 16382: //Grimoire of Devour Magic (Rank 3)
+		case 16383: //Grimoire of Devour Magic (Rank 4)
+		case 22188: //Grimoire of Devour Magic (Rank 5)
+		case 22189: //Grimoire of Devour Magic (Rank 6)
+		case 16390: //Grimoire of Paranoia
+		case 16388: //Grimoire of Spell Lock (Rank 1)
+		case 16389: //Grimoire of Spell Lock (Rank 2)
+		case 16384: //Grimoire of Tainted Blood (Rank 1)
+		case 16385: //Grimoire of Tainted Blood (Rank 2)
+		case 16386: //Grimoire of Tainted Blood (Rank 3)
+		case 16387: //Grimoire of Tainted Blood (Rank 4)
+		case 22190: //Grimoire of Tainted Blood (Rank 5)
+			//Felhunter
+			pItemPrototype->ForcedPetId = 417;
 			break;
 
 		case 21283:
@@ -667,6 +678,7 @@ void Storage_LoadAdditionalTables()
 		}
 	}
 }
+
 
 
 
