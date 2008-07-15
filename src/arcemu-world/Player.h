84cd0f4e5f9f2edc07243cc045f7d62ea3095e80
by-shady <by-shady@arcemu.org>
2008-07-15 15:23:49 +0000

    *Internal: compile fixes.
    PS. btw 2 last patches was by emsy

--- src/arcemu-world/Player.h
+++ src/arcemu-world/Player.h
@@ -76,6 +76,30 @@ enum Races
 	RACE_DRAENEI = 11,
 };
 
+enum ShapeshiftForm
+{
+	FORM_NORMAL             = 0,
+	FORM_CAT                = 1,
+	FORM_TREE               = 2,
+	FORM_TRAVEL             = 3,
+	FORM_AQUA               = 4,
+	FORM_BEAR               = 5,
+	FORM_AMBIENT            = 6,
+	FORM_GHOUL              = 7,
+	FORM_DIREBEAR           = 8,
+	FORM_CREATUREBEAR       = 14,
+	FORM_GHOSTWOLF          = 16,
+	FORM_BATTLESTANCE       = 17,
+	FORM_DEFENSIVESTANCE    = 18,
+	FORM_BERSERKERSTANCE    = 19,
+	FORM_SWIFT              = 27,
+	FORM_SHADOW             = 28,
+	FORM_FLIGHT             = 29,
+	FORM_STEALTH            = 30,
+	FORM_MOONKIN            = 31,
+	FORM_SPIRITOFREDEMPTION = 32,
+};
+
 enum PlayerStatus
 {
 	NONE			 = 0,
