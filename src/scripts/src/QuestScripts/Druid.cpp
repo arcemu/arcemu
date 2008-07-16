ed812c4e486e0e4bb6b2064ede87b47de203828a
neo_mat <neo_mat@arcemu.org>
2008-07-16 02:28:30 +0000

    Little version update

--- src/scripts/src/QuestScripts/Druid.cpp
+++ src/scripts/src/QuestScripts/Druid.cpp
@@ -1,8 +1,10 @@
 /*
- * WEmu Scripts for WEmu MMORPG Server
+ * ArcEmu Scripts for ArcEmu MMORPG Server
+ * Copyright (C) 2008 ArcEmu Team
+ * Based on WEmu Scripts for WEmu MMORPG Server
  * Copyright (C) 2008 WEmu Team
- * Based on Moon++ Scripts for arcemu MMORPG Server
- * Copyright (C) 2005-2008 arcemu Team
+ * Based on Moon++ Scripts for Ascent MMORPG Server
+ * Copyright (C) 2005-2008 Ascent Team
  * Copyright (C) 2007-2008 Moon++ Team
  *
  * This program is free software: you can redistribute it and/or modify
