1a9b538a4cc82c18a3a0b4183a5c20591f52dc3f
Vojta <Vojta@arcemu.org>
2008-08-19 12:25:20 +0000

    * forgot SERVER_DECL MapCell, sorry about that

--- src/arcemu-world/MapCell.h
+++ src/arcemu-world/MapCell.h
@@ -29,7 +29,7 @@ class Map;
 #define MAKE_CELL_EVENT(x,y) ( ((x) * 1000) + 200 + y )
 #define DECODE_CELL_EVENT(dest_x, dest_y, ev) (dest_x) = ((ev-200)/1000); (dest_y) = ((ev-200)%1000);
 
-class MapCell
+class SERVER_DECL MapCell
 {
 	friend class MapMgr;
 public:
