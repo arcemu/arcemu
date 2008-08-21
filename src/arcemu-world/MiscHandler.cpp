6ee7f82b11948bd91f67bea1a17434df1fef5a7e
Hasbro <Hasbro@arcemu.org>
2008-08-21 02:16:18 +0000

    Crash fix ported from AspireCore.

--- src/arcemu-world/MiscHandler.cpp
+++ src/arcemu-world/MiscHandler.cpp
@@ -1674,6 +1674,8 @@ void WorldSession::HandleSetActionBarTogglesOpcode(WorldPacket &recvPacket)
 // Handlers for acknowledgement opcodes (removes some 'unknown opcode' flood from the logs)
 void WorldSession::HandleAcknowledgementOpcodes( WorldPacket & recv_data )
 {
+	if(!_player)
+		return;
 	switch(recv_data.GetOpcode())
 	{
 	case CMSG_MOVE_WATER_WALK_ACK:
