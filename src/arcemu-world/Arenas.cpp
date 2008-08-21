5a700b4cb431e09e66d300cbd85ac42314ff70e0
Marcelo <Marcelo@arcemu.org>
2008-08-21 23:23:03 +0000

    Wasn't meant to commit Arenas.cpp on last commit, sorry

--- src/arcemu-world/Arenas.cpp
+++ src/arcemu-world/Arenas.cpp
@@ -66,6 +66,13 @@ void Arena::OnAddPlayer(Player * plr)
 
 	if( !m_started )
 	{
+		/* cast arena readyness buff */
+		if(plr->isDead())
+			plr->ResurrectPlayer();
+
+		plr->SetUInt32Value(UNIT_FIELD_HEALTH, plr->GetUInt32Value(UNIT_FIELD_MAXHEALTH));
+		plr->SetUInt32Value(UNIT_FIELD_POWER1, plr->GetUInt32Value(UNIT_FIELD_MAXPOWER1));
+		plr->SetUInt32Value(UNIT_FIELD_POWER4, plr->GetUInt32Value(UNIT_FIELD_MAXPOWER4));
 		sEventMgr.AddEvent(plr, &Player::FullHPMP, EVENT_PLAYER_UPDATE, 500, 1, 0);
 		sEventMgr.AddEvent(plr, &Player::ResetAllCooldowns, EVENT_PLAYER_UPDATE, 500, 1, 0);
 	}
@@ -668,7 +675,6 @@ void Player::FullHPMP()
 {
 	if(isDead())
 		ResurrectPlayer();
-	sLog.outDebug("%s: setting health %d", __FUNCTION__, GetUInt32Value(UNIT_FIELD_MAXHEALTH));
     SetUInt32Value(UNIT_FIELD_HEALTH, GetUInt32Value(UNIT_FIELD_MAXHEALTH));
     SetUInt32Value(UNIT_FIELD_POWER1, GetUInt32Value(UNIT_FIELD_MAXPOWER1));
     SetUInt32Value(UNIT_FIELD_POWER4, GetUInt32Value(UNIT_FIELD_MAXPOWER4));
