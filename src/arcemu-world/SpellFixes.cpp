cada00dbf9b5f1d90dd24969194b25a49a7fbac2
Marcelo <Marcelo@arcemu.org>
2008-08-24 18:59:18 +0000

    [Mage] Winter's Chill was procing on self while casting Summon Water Elemental

--- src/arcemu-world/SpellFixes.cpp
+++ src/arcemu-world/SpellFixes.cpp
@@ -13774,6 +13774,23 @@ void ApplyNormalFixes()
 		if ( sp != NULL )
 			sp->EffectImplicitTargetA[0] = EFF_TARGET_DYNAMIC_OBJECT;
 
+		// Mage - Winter's Chill
+		sp = dbcSpell.LookupEntryForced( 11180 );
+		if( sp != NULL )
+			sp->procFlags = PROC_ON_SPELL_HIT;
+		sp = dbcSpell.LookupEntryForced( 28592 );
+		if( sp != NULL )
+			sp->procFlags = PROC_ON_SPELL_HIT;
+		sp = dbcSpell.LookupEntryForced( 28593 );
+		if( sp != NULL )
+			sp->procFlags = PROC_ON_SPELL_HIT;
+		sp = dbcSpell.LookupEntryForced( 28594 );
+		if( sp != NULL )
+			sp->procFlags = PROC_ON_SPELL_HIT;
+		sp = dbcSpell.LookupEntryForced( 28595 );
+		if( sp != NULL )
+			sp->procFlags = PROC_ON_SPELL_HIT;
+
 	//////////////////////////////////////////
 	// WARLOCK								//
 	//////////////////////////////////////////
