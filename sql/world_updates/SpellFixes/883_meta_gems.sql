04cf7cd5e00658a4d6677dbd353725e212e43322
NeoMat <NeoMat@arcemu.org>
2008-08-26 01:53:40 +0000

    SQL Update,Apply it to your DB! Items: - Brutal Earthstorm Diamond - Insightful Earthstorm Diamond

---
+++ sql/world_updates/SpellFixes/883_meta_gems.sql
@@ -0,0 +1,4 @@
+-- Brutal Earthstorm Diamond
+REPLACE INTO spellfixes(spellId, procChance) VALUES(37982, 1);
+-- Insightful Earthstorm Diamond
+REPLACE INTO spellfixes(spellId, procChance) VALUES(27521, 5);
