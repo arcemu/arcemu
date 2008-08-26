c8a2dbb49851bcc5b6c0eb2dc591bdb65bd0cca3
Maven <Maven@arcemu.org>
2008-08-26 01:36:44 +0000

    Fixes, and Added function. Now you when you edit your saved announces, you can also edit where to send them. Currently you can only pick one place to send at a time.

--- extras/GMH/AnnounceScript.lua
+++ extras/GMH/AnnounceScript.lua
@@ -1,19 +1,87 @@
-﻿function FirstAnnounce()
+﻿function SetAnnouncementChecked()
+if (AnnounceCheck:GetChecked() or ScreenCheck:GetChecked() or GMAnnounceCheck:GetChecked() ) then
+Announce();
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end
+end
+
+function FirstAnnounce()
+if (AnnounceSetCheck:GetChecked() or ScreenAnnounceSetCheck:GetChecked() or GMAnnounceSetCheck:GetChecked() ) then
+if AnnounceSetCheck:GetChecked() then
 firstannounce=".announce "..SetAnnounceText:GetText();    
+end
+if ScreenAnnounceSetCheck:GetChecked() then
+firstannounce=".wannounce "..SetAnnounceText:GetText();    
+end
+if GMAnnounceSetCheck:GetChecked() then
+firstannounce=".gmannounce "..SetAnnounceText:GetText();    
+end
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end
 end
 
 function SecondAnnounce()
+if (AnnounceSetCheck:GetChecked() or ScreenAnnounceSetCheck:GetChecked() or GMAnnounceSetCheck:GetChecked() ) then
+if AnnounceSetCheck:GetChecked() then
 secondannounce=".announce "..SetAnnounceText:GetText();    
+end
+if ScreenAnnounceSetCheck:GetChecked() then
+secondannounce=".wannounce "..SetAnnounceText:GetText();    
+end
+if GMAnnounceSetCheck:GetChecked() then
+secondannounce=".gmannounce "..SetAnnounceText:GetText();    
+end
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end  
 end
 
 function ThirdAnnounce()
+if (AnnounceSetCheck:GetChecked() or ScreenAnnounceSetCheck:GetChecked() or GMAnnounceSetCheck:GetChecked() ) then
+if AnnounceSetCheck:GetChecked() then
 thirdannounce=".announce "..SetAnnounceText:GetText();    
+end
+if ScreenAnnounceSetCheck:GetChecked() then
+thirdannounce=".wannounce "..SetAnnounceText:GetText();    
+end
+if GMAnnounceSetCheck:GetChecked() then
+thirdannounce=".gmannounce "..SetAnnounceText:GetText();    
+end
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end     
 end
 
 function ForthAnnounce()
+if (AnnounceSetCheck:GetChecked() or ScreenAnnounceSetCheck:GetChecked() or GMAnnounceSetCheck:GetChecked() ) then
+if AnnounceSetCheck:GetChecked() then
 forthannounce=".announce "..SetAnnounceText:GetText();    
+end
+if ScreenAnnounceSetCheck:GetChecked() then
+forthannounce=".wannounce "..SetAnnounceText:GetText();    
+end
+if GMAnnounceSetCheck:GetChecked() then
+forthannounce=".gmannounce "..SetAnnounceText:GetText();    
+end
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end     
 end
 
 function FifthAnnounce()
+if (AnnounceSetCheck:GetChecked() or ScreenAnnounceSetCheck:GetChecked() or GMAnnounceSetCheck:GetChecked() ) then
+if AnnounceSetCheck:GetChecked() then
 fifthannounce=".announce "..SetAnnounceText:GetText();    
+end
+if ScreenAnnounceSetCheck:GetChecked() then
+fifthannounce=".wannounce "..SetAnnounceText:GetText();    
+end
+if GMAnnounceSetCheck:GetChecked() then
+fifthannounce=".gmannounce "..SetAnnounceText:GetText();    
+end
+else
+UIErrorsFrame:AddMessage("Please choose where to Announce!", 1.0, 0.0, 0.0, 53, 2);
+end     
 end
--- extras/GMH/Skill2Script.lua
+++ extras/GMH/Skill2Script.lua
@@ -1,36 +1,48 @@
-﻿function LearnProf()
-     if ( ProfessionComboButton:GetText() == Alchemy ) then
-     result=".character advancesk 171 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Blacksmithing ) then
-     result=".character advancesk 164 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Enchanting ) then
-     result=".character advancesk 333 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Engineering ) then
-     result=".character advancesk 202 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Herbalism ) then
-     result=".character advancesk 182 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Jewelcrafting ) then
-     result=".character advancesk 755 "..SkillLvl:GetText(); 
-     elseif ( ProfessionComboButton:GetText() == Leatherworking ) then
-     result=".character advancesk 165 "..SkillLvl:GetText(); 
-     elseif ( ProfessionComboButton:GetText() == Mining ) then
-     result=".character advancesk 186 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Skinning ) then
-     result=".character advancesk 393 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Tailoring ) then
-     result=".character advancesk 197 "..SkillLvl:GetText(); 
-     elseif ( ProfessionComboButton:GetText() == Cooking ) then
-     result=".character advancesk 185 "..SkillLvl:GetText();  
-     elseif ( ProfessionComboButton:GetText() == Fishing ) then
-     result=".character advancesk 356 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == FirstAid ) then
-     result=".character advancesk 129 "..SkillLvl:GetText();
-     elseif ( ProfessionComboButton:GetText() == Poisons ) then
-     result=".character advancesk 40 "..SkillLvl:GetText(); 
-     else
-     result="bloody error...";
-     end
+﻿function LearnProf123()
+     if (ProfessionComboButton:GetText() == Alchemy) then
+     result=".character advancesk 171 "..SkillLevel1:GetText();
      outSAY(result);
+     elseif ( ProfessionComboButton:GetText() == Blacksmithing ) then
+     result=".character advancesk 164 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Enchanting) then
+     result=".character advancesk 333 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Engineering) then
+     result=".character advancesk 202 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Jewelcrafting) then
+     result=".character advancesk 755 "..SkillLevel1:GetText(); 
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Leatherworking) then
+     result=".character advancesk 165 "..SkillLevel1:GetText(); 
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Mining) then
+     result=".character advancesk 186 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Skinning) then
+     result=".character advancesk 393 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Tailoring) then
+     result=".character advancesk 197 "..SkillLevel1:GetText(); 
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Cooking) then
+     result=".character advancesk 185 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Fishing) then
+     result=".character advancesk 356 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == FirstAid) then
+     result=".character advancesk 129 "..SkillLevel1:GetText();
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Poisons) then
+     result=".character advancesk 40 "..SkillLevel1:GetText(); 
+     outSAY(result);
+     elseif (ProfessionComboButton:GetText() == Herbalism ) then
+     result=".character advancesk 182 "..SkillLevel1:GetText();
+     outSAY(result);
+     end
+
 end
 --[[Alchemy
 Blacksmithing
