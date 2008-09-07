4ab11d1051cde84292860bec07dee59cfc3b3e2b
NeoMat <NeoMat@arcemu.org>
2008-09-07 14:44:02 +0000

    PvPToken script,thx to HalestormXV for idea and provided script (http://arcemu.org/forums/index.php?showtopic=2544)

--- src/scripts/src/ExtraScripts/PvPToken.cpp
+++ src/scripts/src/ExtraScripts/PvPToken.cpp
@@ -1,29 +1,34 @@
+//Made by ???
+//Modified by WigSplitta whole code
+//Re-Modified by HalestormXV (Added More Features)
+//Random Dice Concept to Kuraria for Helping HalestormXV
+//Modified by Neo_mat
+
 #include "StdAfx.h"
 #include "Setup.h"
 
-void onPvpKill(Player* plr, Player* victim)
-{
-	if( plr->GetTeam() != victim->GetTeam())
-	{
-		char message[200];
-		sprintf(message, "%s has killed %s", plr->GetName(), victim->GetName());
-		sWorld.SendWorldWideScreenText(message);
+#define TokenID 19712 //set token id here
 
-		int32 gold_check_p = plr->GetUInt32Value( PLAYER_FIELD_COINAGE );
-		int32 gold_check_v = victim->GetUInt32Value( PLAYER_FIELD_COINAGE );
-		int32 new_gold_p = gold_check_p + 10000;
-		int32 new_gold_v;
-		if(gold_check_v < 5000)
+void onPvpKill(Player* pPlayer)
+{
+	int die;
+	die = rand()%5 + 1;
+	if (die <= 1)
+	{
+		Item *item;
+		item = objmgr.CreateItem( TokenID , pPlayer);
+		item->SetUInt32Value(ITEM_FIELD_STACK_COUNT,1);
+		item->SoulBind();
+		if(!pPlayer->GetItemInterface()->AddItemToFreeSlot(item))
 		{
-			new_gold_v = 0;
+			pPlayer->GetSession()->SendNotification("No free slots were found in your inventory!");
+			item->DeleteMe();
 		}
 		else
 		{
-			new_gold_v = gold_check_v - 5000;
+			SlotResult *lr = pPlayer->GetItemInterface()->LastSearchResult();
+			pPlayer->GetSession()->SendItemPushResult(item,false,true,false,true,lr->ContainerSlot,lr->Slot,1);
 		}
-
-		plr->SetUInt32Value( PLAYER_FIELD_COINAGE, new_gold_p );
-		victim->SetUInt32Value( PLAYER_FIELD_COINAGE, new_gold_v );
 	}
 }
 
