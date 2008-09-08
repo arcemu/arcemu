23d906a1eb2790766148cece699648a5a170ea91
Emsy <Emsy@arcemu.org>
2008-09-08 12:45:19 +0000

    * increasing weapon skills was too slow >.> (anyone knows proper formula btw? in best case one which includes intellect bonus too)

--- src/arcemu-world/Player.cpp
+++ src/arcemu-world/Player.cpp
@@ -9699,7 +9699,7 @@ void Player::_ModifySkillBonusByType(uint32 SkillType, int32 Delta)
 float PlayerSkill::GetSkillUpChance()
 {
 	float diff = float(MaximumValue - CurrentValue);
-	return (diff * 100.0f / float(MaximumValue)) / 3.0f;
+	return (diff * 100.0f / float(MaximumValue));
 }
 
 void Player::_RemoveLanguages()
