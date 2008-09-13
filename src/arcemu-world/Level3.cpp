57907eca20d9ef0f5e6f0a1d58ac85e1e6f2cffc
Marcelo <Marcelo@arcemu.org>
2008-09-13 04:45:30 +0000

    spellfixes can now be reloaded with .server reloadtable spellfixes

--- src/arcemu-world/Level3.cpp
+++ src/arcemu-world/Level3.cpp
@@ -1161,6 +1161,11 @@ bool ChatHandler::HandleDBReloadCommand(const char* args, WorldSession* m_sessio
 		objmgr.ReloadDisabledSpells();
 		ret = 1;
 	} else
+	if (0 == stricmp(args, "spellfixes"))
+	{
+		objmgr.LoadSpellFixes();
+		ret = 1;
+	} else
 	if (0 == stricmp(args, "vendors"))
 	{
 		objmgr.ReloadVendors();
