33a1131676460ab0baf9d6276cb670d0a6be89c4
Marcelo <Marcelo@arcemu.org>
2008-09-14 01:38:13 +0000

    Fixed [Warlock] Mana Feed, [Paladin] Divine Favor, [Paladin] Improved Concentration Aura and missing rank from Improved Bloodrage. I didn't add another .sql, just rerun 1016_spellfixes.sql

--- sql/world_updates/SpellFixes/1016_spellfixes.sql
+++ sql/world_updates/SpellFixes/1016_spellfixes.sql
@@ -28,3 +28,22 @@ INSERT INTO spellfixes(spellId, groupRelation0) VALUES(19462, 0x3000) ON DUPLICA
 INSERT INTO spellfixes(spellId, groupRelation1) VALUES(19462, 0x3000) ON DUPLICATE KEY UPDATE groupRelation1=VALUES(groupRelation1);
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(24691, 0x3000) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 INSERT INTO spellfixes(spellId, groupRelation1) VALUES(24691, 0x3000) ON DUPLICATE KEY UPDATE groupRelation1=VALUES(groupRelation1);
+
+-- [Warlock] Mana Feed
+INSERT INTO spellfixes(spellId, groupRelation0) VALUES(30326, 0x40010) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
+INSERT INTO spellfixes(spellId, groupRelation0) VALUES(30327, 0x40010) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
+INSERT INTO spellfixes(spellId, groupRelation0) VALUES(30328, 0x40010) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
+
+-- [Paladin] Divine Favor
+INSERT INTO spellfixes(spellId, groupRelation0) VALUES(20216, 0x10000C0200000) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
+
+-- [Paladin] Improved Concentration Aura	
+INSERT INTO spellfixes(spellId, groupRelation1) VALUES(20254, 0x20000) ON DUPLICATE KEY UPDATE groupRelation1=VALUES(groupRelation1);
+INSERT INTO spellfixes(spellId, groupRelation2) VALUES(20254, 0x20000) ON DUPLICATE KEY UPDATE groupRelation2=VALUES(groupRelation2);
+INSERT INTO spellfixes(spellId, groupRelation1) VALUES(20255, 0x20000) ON DUPLICATE KEY UPDATE groupRelation1=VALUES(groupRelation1);
+INSERT INTO spellfixes(spellId, groupRelation2) VALUES(20255, 0x20000) ON DUPLICATE KEY UPDATE groupRelation2=VALUES(groupRelation2);
+INSERT INTO spellfixes(spellId, groupRelation1) VALUES(20256, 0x20000) ON DUPLICATE KEY UPDATE groupRelation1=VALUES(groupRelation1);
+INSERT INTO spellfixes(spellId, groupRelation2) VALUES(20256, 0x20000) ON DUPLICATE KEY UPDATE groupRelation2=VALUES(groupRelation2);
+
+-- Improved Bloodrage
+INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12301, 0x100) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
