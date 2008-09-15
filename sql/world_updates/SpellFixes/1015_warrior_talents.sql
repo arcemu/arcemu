2a20a723069c742f602484ac3a206598fc80f0e1
Marcelo <Marcelo@arcemu.org>
2008-09-15 14:24:33 +0000

    http://arcemu.org/forums/index.php?showtopic=4470 yay, i didn't notice that :) (thx to Kenjiro)

--- sql/world_updates/SpellFixes/1015_warrior_talents.sql
+++ sql/world_updates/SpellFixes/1015_warrior_talents.sql
@@ -1,14 +1,14 @@
-; Improved Overpower
+-- Improved Overpower
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12290, 0x4) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12963, 0x4) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 
-; Improved Charge
+-- Improved Charge
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12285, 0x1) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12697, 0x1) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 
-; Improved Bloodrage
+-- Improved Bloodrage
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(12818, 0x100) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 
-; Impale
+-- Impale
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(16493, 0x14D2A600CEF) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
 INSERT INTO spellfixes(spellId, groupRelation0) VALUES(16494, 0x14D2A600CEF) ON DUPLICATE KEY UPDATE groupRelation0=VALUES(groupRelation0);
