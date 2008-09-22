59a2989b0016af6a7f371cae9fbb0babe54d7dc3
Zack <Zack@arcemu.org>
2008-09-22 18:36:00 +0000

    let's avoid that access violation too if possible

--- src/arcemu-world/CellHandler.h
+++ src/arcemu-world/CellHandler.h
@@ -163,7 +163,7 @@ void CellHandler<Class>::Remove(uint32 x, uint32 y)
 template <class Class>
 Class* CellHandler<Class>::GetCell(uint32 x, uint32 y)
 {
-	if(!_cells[x] || x >= _sizeX || y >= _sizeY ) return NULL;
+	if( x >= _sizeX || y >= _sizeY || !_cells[x] ) return NULL;
 	return _cells[x][y];
 }
 
