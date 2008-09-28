fe35b873db050a92474f8a974cdd7800a176cfe7
Misz <Misz@arcemu.org>
2008-09-28 16:29:15 +0000

    Moving

--- src/ArcScript/Archive/Lua scripts/GO - The Hinterlands - Lards Picnic Basket.lua
+++
@@ -1,8 +0,0 @@
-
-
-function Lards_Picnic_Basket_onUse (pUnit, Event)
-	pUnit:SpawnCreature (14748, 401.681061, -4825.897949, 9.381310, 3.16951, 95, 540000)
-	pUnit:SpawnCreature (14748, 403.259125, -4830.528320, 8.991310, 3.16951, 95, 540000)
-	pUnit:SpawnCreature (14748, 404.802185, -4820.998535, 10.790082, 3.16951, 95, 540000)
-end
-RegisterGameObjectEvent (179910, 2, "Lards_Picnic_Basket_onUse")
