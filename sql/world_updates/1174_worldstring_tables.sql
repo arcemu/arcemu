2a147b9ca787dbf884f3b84e82f416ef9806ad54
Cebernic <Cebernic@arcemu.org>
2008-10-04 18:11:35 +0000

    Revision 1174 SQL files. plz feedback....btw for guys from china: if you cannot access http://arcemu.org/forums/ just leave your message here ->  http://home.p2wow.com/bbs/viewthread.php?tid=370 [chinese only]

---
+++ sql/world_updates/1174_worldstring_tables.sql
@@ -0,0 +1,16 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worldstring_tables`
+--
+
+CREATE TABLE IF NOT EXISTS `worldstring_tables` (
+  `entry` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`entry`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `worldstring_tables`
+--
+-- cebernic: The Contents I'll be inserted as soon as possible.
