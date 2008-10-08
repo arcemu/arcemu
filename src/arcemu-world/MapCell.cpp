93c791b30f6baf4efdaf936bdb8fc83bdc5a3514
Marcelo <Marcelo@arcemu.org>
2008-10-08 01:10:37 +0000

    Crashfix by 0x0f7afc68

--- src/arcemu-world/MapCell.cpp
+++ src/arcemu-world/MapCell.cpp
@@ -131,22 +131,8 @@ void MapCell::RemoveObjects()
 
 		itr++;
 
-		if(!obj)
+		if(!obj || _unloadpending)
 			continue;
-
-		if( _unloadpending )
-		{
-			if(obj->GetTypeFromGUID() == HIGHGUID_TYPE_TRANSPORTER)
-				continue;
-
-			if(obj->GetTypeId()==TYPEID_CORPSE && obj->GetUInt32Value(CORPSE_FIELD_OWNER) != 0)
-				continue;
-
-			if(!obj->m_loadedFromDB)
-				continue;
-		}
-
-
 
 		if( obj->Active )
 			obj->Deactivate( _mapmgr );
