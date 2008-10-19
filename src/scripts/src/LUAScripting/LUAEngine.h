6a5b95993eb3b8dfe64ee17792789f3517b74c89
Cebernic <Cebernic@arcemu.org>
2008-10-19 04:48:46 +0000

    Rename script_lua/ to scripts/  [subdirloader] just let name same as *nix subdirloader is Win32 platform only.

--- src/scripts/src/LUAScripting/LUAEngine.h
+++ src/scripts/src/LUAScripting/LUAEngine.h
@@ -124,12 +124,17 @@ class LuaEngine
 private:
 	lua_State * L;
 	Mutex m_Lock;
+  set<string> luaFiles;
+  set<string> luaBytecodeFiles;
 
 public:
 	LuaEngine();
 	~LuaEngine();
 
 	void LoadScripts();
+#ifdef WIN32
+	void ScriptLoadFromDir(char* Dirname);
+#endif
 	void Shutdown();
 	void Restart();
 	void RegisterCoreFunctions();
