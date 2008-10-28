fffc2230e29cfa6033aad6204377a9cf6c0e6d93
Marcelo <Marcelo@arcemu.org>
2008-10-28 13:58:37 +0000

    Merging some spellfixes sql's into only one file spellfixes.sql, this should be easier whenever we update spellfixes

--- sql/world_updates/SpellFixes/883_meta_gems.sql
+++
@@ -1,4 +0,0 @@
--- Brutal Earthstorm Diamond
-REPLACE INTO spellfixes(spellId, procChance) VALUES(37982, 1);
--- Insightful Earthstorm Diamond
-REPLACE INTO spellfixes(spellId, procChance) VALUES(27521, 5);
