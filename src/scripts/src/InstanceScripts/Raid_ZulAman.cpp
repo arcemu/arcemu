1627e016f52100dffd8d0506b0f5196fbf7050fd
Swiftsmoke <Swiftsmoke@arcemu.org>
2008-11-09 12:28:51 +0000

    - http://arcemu.org/forums/index.php?showtopic=7460
    Thanks to SleepCoder

--- src/scripts/src/InstanceScripts/Raid_ZulAman.cpp
+++ src/scripts/src/InstanceScripts/Raid_ZulAman.cpp
@@ -1966,16 +1966,16 @@ class DARKHEARTAI : public CreatureAIScript
 {
 public:
     ADD_CREATURE_FACTORY_FUNCTION(DARKHEARTAI);
-	SP_AI_Spell spells[2];
-	bool m_spellcheck[2];
+	SP_AI_Spell spells[1];
+	bool m_spellcheck[1];
 
     DARKHEARTAI(Creature* pCreature) : CreatureAIScript(pCreature)
     {
-			nrspells = 2;
-			for(int i=0;i<nrspells;i++)
-			{
-				m_spellcheck[i] = false;
-			}
+		nrspells = 1;
+		for(int i=0;i<nrspells;i++)
+		{
+			m_spellcheck[i] = false;
+		}
 		
         spells[0].info = dbcSpell.LookupEntry(DH_CLEAVE);
 		spells[0].targettype = TARGET_ATTACKING;
