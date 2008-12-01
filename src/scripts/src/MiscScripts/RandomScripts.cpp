7b19cd9800a823df324659bc4034a8354eaf9141
Feanor <Feanor@arcemu.org>
2008-12-01 22:44:32 +0000

    Lies, commited SSC Doors script now...

--- src/scripts/src/MiscScripts/RandomScripts.cpp
+++ src/scripts/src/MiscScripts/RandomScripts.cpp
@@ -76,7 +76,36 @@ void OnEmote(Player * pPlayer, uint32 Emote, Unit * pUnit)
 	}
 }
 
+void SSCDoors(Player * pPlayer)
+{
+	//Only opens when the first one steps in, if 669 if you find a way, put it in :P (else was used to increase the time the door stays opened when another one steps on it)
+	GameObject *door = pPlayer->GetMapMgr()->GetInterface()->GetGameObjectNearestCoords(803.827f, 6869.38f, -38.5434f, 184212);
+	if (door && (door->GetByte(GAMEOBJECT_BYTES_1, 0) == 1))
+	{
+		door->SetByte(GAMEOBJECT_BYTES_1, 0, 0);
+		//sEventMgr.AddEvent(door, &GameObject::SetUInt32Value, GAMEOBJECT_STATE, 1, EVENT_SCRIPT_UPDATE_EVENT, 10000, 1, 0);
+	}
+	//else
+	//{
+		//sEventMgr.RemoveEvents(door);
+		//sEventMgr.AddEvent(door, &GameObject::SetUInt32Value,GAMEOBJECT_STATE, 0, EVENT_SCRIPT_UPDATE_EVENT, 10000, 1, 0);
+	//}
+}
+
+void OnAreaTrigger(Player * pPlayer, uint32 AreaTrigger)
+{
+	switch(AreaTrigger)
+	{
+	case 4591:
+		{
+			SSCDoors(pPlayer);
+		}break;
+	}
+}
+
+
 void SetupRandomScripts(ScriptMgr * mgr)
 {	// Register Hook Event here
 	mgr->register_hook(SERVER_HOOK_EVENT_ON_EMOTE, (void *)&OnEmote);
+	mgr->register_hook(SERVER_HOOK_EVENT_ON_AREATRIGGER, (void *)&OnAreaTrigger);
 }
