902eeab5e36cf57a0c6d194ac4625166136cb6a6
Feanor <Feanor@arcemu.org>
2008-12-03 15:37:27 +0000

    WHERE statement so it wont overwrite Your changes.

--- sql/world_updates/1735_quests.sql
+++ sql/world_updates/1735_quests.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `quests` ADD `RemoveQuests` longtext NOT NULL AFTER `RequiredQuest4`;
-UPDATE `quests` set `RemoveQuests` = "";
+UPDATE `quests` set `RemoveQuests` = "" WHERE `RemoveQuests` IS NULL;
