5b454e0b08a6f4ec24eb559918faa87296fbe9bc
Maven <Maven@arcemu.org>
2008-12-03 01:57:18 +0000

    Fixed DB editor. user and pass were declared 3 times when they only needed to declared once. Also fixed some typos and added the news spawns sql
    And yes...this has been tested work.
    
    *DEV'S PLEASE UPDATE THIS WHENEVER YOU ADD A NEW SQL UPDATE* Thanks

--- sql/ArcEmu DB Updater.bat
+++ sql/ArcEmu DB Updater.bat
@@ -2,7 +2,7 @@
 REM ############################################################################
 REM # Credits 
 REM # Base Script by NCDB http://www.nc-db.info
-REM # Modified by MrHooHa ArcEmu http://www.arcemu.org
+REM # Modified by MrHooHa and Maven ArcEmu http://www.arcemu.org
 REM ############################################################################
 REM # 
 REM #      B A S I C   U S E R   C O N F I G U R A T I O N   A R E A
@@ -13,22 +13,17 @@ REM # server - MySql Host
 REM # user   - MySql Username
 REM # pass   - MySql Password
 REM # logon  - Logon Database
-REM # chr    - Characture Database
+REM # chr    - Character Database
 REM # wdb    - World Database
 REM #########################################
+REM MySQL Username and Password
+set user=changeme
+set pass=changeme
 REM # Logon Database
-set user=changeme
-set pass=changeme
 set logon=changeme
-REM #########################################
-REM # Characture Database
-set user=changeme
-set pass=changeme
-set chr= changeme
-REM #########################################
+REM # Character Database
+set chr=changeme
 REM # World Database
-SET user=changeme
-set pass=changeme
 set wdb=changeme
 REM ############################################################################
 REM #
@@ -42,16 +37,16 @@ REM #
 REM #     D O   N O T   M O D I F Y   B E Y O N D   T H I S   P O I N T
 REM #
 REM ############################################################################
-if %user% == CHANGEME GOTO error2
-if %pass% == CHANGEME GOTO error2
+if %user% == changeme GOTO error2
+if %pass% == changeme GOTO error2
 :menu
 cls
 ECHO.
 ECHO.
-Echo	 ####################################################
-ECHO	 #    Base Script by NCDB http://www.nc-db.info     #
-ECHO	 # Modified by MrHooHa ArcEmu http://www.arcemu.org #
-ECHO	 ####################################################
+Echo	 ##############################################################
+ECHO	 #    	   Base Script by NCDB http://www.nc-db.info          #
+ECHO	 # Modified by MrHooHa and Maven ArcEmu http://www.arcemu.org #
+ECHO	 ##############################################################
 ECHO		 ####################################
 ECHO		 #######    ArcEmu Database   #######
 ECHO		 ######        Revision        ######
@@ -61,7 +56,7 @@ ECHO.
 ECHO		 Please type the letter for the option:
 ECHO.
 ECHO		  W = Install World Database updates
-ECHO		  C = Install Charature updates
+ECHO		  C = Install Character updates
 ECHO		  L = Install Logon updates
 ECHO.
 ECHO.
@@ -139,6 +134,8 @@ ECHO [Importing] 1729_creature_proto.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %wdb% < world_updates\1729_creature_proto.sql
 ECHO [Importing] 1735_quests.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %wdb% < world_updates\1735_quests.sql
+ECHO [Importing] 1746_spawns.sql
+mysql -h %server% --user=%user% --password=%pass% --port=%port% %wdb% < world_updates\1746_spawns.sql
 ECHO [Importing] spellfixes.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %wdb% < world_updates\spellfixes.sql
 ECHO [Importing] Finished
@@ -151,7 +148,7 @@ CLS
 ECHO.
 ECHO.
 ECHO [Importing] Started...
-ECHO [Importing] ArcEmu Characture Updates ...
+ECHO [Importing] ArcEmu Character Updates ...
 ECHO [Importing] 147_pet_untrain_cost.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %chr% < character_updates\147_pet_untrain_cost.sql
 ECHO [Importing] 261_gm_tickets.sql
@@ -189,7 +186,7 @@ CLS
 ECHO.
 ECHO.
 ECHO [Importing] Started...
-ECHO [Importing] ArcEmu Characture Updates ...
+ECHO [Importing] ArcEmu Character Updates ...
 ECHO [Importing] 1066_add_banreasons.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %logon% < logon_updates\1066_add_banreasons.sql
 ECHO [Importing] correction_to_ipbans(allrevs).sql
