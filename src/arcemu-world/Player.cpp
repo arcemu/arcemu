8abb8c7269644c74ddbdf445a90055b417b33df6
SBAL <SBAL@arcemu.org>
2008-12-11 20:59:23 +0000

    * Forgotten change of field numbers

--- src/arcemu-world/Player.cpp
+++ src/arcemu-world/Player.cpp
@@ -1935,12 +1935,12 @@ void Player::_LoadPet(QueryResult * result)
 		pet->active  = fields[6].GetInt8()%10 > 0 ? true : false;
 		pet->stablestate = fields[6].GetInt8() / 10;
 		pet->level   = fields[7].GetUInt32();
-		pet->actionbar = fields[9].GetString();
-		pet->happinessupdate = fields[10].GetUInt32();
-		pet->summon = (fields[11].GetUInt32()>0 ? true : false);
-		pet->reset_time = fields[12].GetUInt32();
-		pet->reset_cost = fields[13].GetUInt32();
-		pet->spellid = fields[14].GetUInt32();
+		pet->actionbar = fields[8].GetString();
+		pet->happinessupdate = fields[9].GetUInt32();
+		pet->summon = (fields[10].GetUInt32()>0 ? true : false);
+		pet->reset_time = fields[11].GetUInt32();
+		pet->reset_cost = fields[12].GetUInt32();
+		pet->spellid = fields[13].GetUInt32();
 
 		m_Pets[pet->number] = pet;
 		if(pet->active)
