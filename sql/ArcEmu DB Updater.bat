68f025ea54963f0a527d585279879bd351e4a4e6
SBAL <SBAL@arcemu.org>
2008-12-11 20:40:16 +0000

    * Once again, do not forget the SQL update from prev. commit

--- sql/ArcEmu DB Updater.bat
+++ sql/ArcEmu DB Updater.bat
@@ -313,6 +313,8 @@ ECHO [Importing] 1564_removed_spells.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %chr% < character_updates\1564_removed_spells.sql
 ECHO [Importing] 1653_achievements.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %chr% < character_updates\1653_achievements.sql
+ECHO [Importing] 1835_playerpets_loyalty.sql
+mysql -h %server% --user=%user% --password=%pass% --port=%port% %chr% < character_updates\1835_playerpets_loyalty.sql
 ECHO [Importing] Character Updates Finished
 ECHO.
 ECHO.
