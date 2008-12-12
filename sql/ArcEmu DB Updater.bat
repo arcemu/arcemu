3b053460a8e4d9abec8fc1bc6bbbcde5e24adbf8
Shyke <Shyke@arcemu.org>
2008-12-12 09:42:51 +0000

    Forgot to update the DB Updater batch.

--- sql/ArcEmu DB Updater.bat
+++ sql/ArcEmu DB Updater.bat
@@ -327,6 +327,8 @@ ECHO [Importing] 1066_add_banreasons.sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %logon% < logon_updates\1066_add_banreasons.sql
 ECHO [Importing] correction_to_ipbans(allrevs).sql
 mysql -h %server% --user=%user% --password=%pass% --port=%port% %logon% < logon_updates\correction_to_ipbans(allrevs).sql
+ECHO [Importing] 1842_Account_Flags.sql
+mysql -h %server% --user=%user% --password=%pass% --port=%port% %logon% < logon_updates\1842_Account_Flags.sql
 ECHO [Importing] Logon Updates Finished
 ECHO  Update Success.
 ECHO [Importing] All Updates Finished
