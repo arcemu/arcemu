ffea841eeed6bb728cffc94e59953dcdf77295ff
BrantX <BrantX@arcemu.org>
2008-12-17 14:06:05 +0000

    Forgot to add this.

--- src/scripts/lua-scripts/WOTLK/Ebon Hold/Teleporters.lua
+++ src/scripts/lua-scripts/WOTLK/Ebon Hold/Teleporters.lua
@@ -1,5 +1,35 @@
+ --[[
+   ******************************************************************
+   *	 _____              ___                           _         *
+   *	(  _  )            (  _`\               _        ( )_       *
+   *	| (_) | _ __   ___ | (_(_)   ___  _ __ (_) _ _   | ,_)      *
+   *	|  _  |( '__)/'___)`\__ \  /'___)( '__)| |( '_`\ | |        *
+   *	| | | || |  ( (___ ( )_) |( (___ | |   | || (_) )| |_       *
+   *	(_) (_)(_)  `\____)`\____)`\____)(_)   (_)| ,__/'`\__)      *
+   *	                                          | |               *
+   *	                                          (_)               *
+   *	                                                            *
+   *	               OpenSource Scripting Team                    *
+   *	                <http://www.arcemu.org>                     *
+   *	                                                            *
+   ******************************************************************
+  
+   This software is provided as free and open source by the
+staff of The ArcScript Project, in accordance with 
+the GPL license. This means we provide the software we have 
+created freely and it has been thoroughly tested to work for 
+the developers, but NO GUARANTEE is made it will work for you 
+as well. Please give credit where credit is due, if modifying,
+redistributing and/or using this software. Thank you.
+
+Staff of ArcScript Project, Dec 2008
+~~End of License Agreement
+
+#############################################################
+Made by: BrantX
+-- ]]
+
 function TeleportDown_AI(pUnit,Event)
--- I am not sure if should loop it or not.
 	pUnit:RegsiterEvent("TeleportDown_DownFront", 1000, 0)
 	pUnit:RegsiterEvent("TeleportDown_DownBack", 1000, 0)
 end
@@ -19,7 +49,6 @@ end
 RegisterUnitEvent(29581, 10, "TeleportDown_AI")
 
 function TeleportUp_AI(pUnit,Event)
--- I am not sure if should loop it or not.
 	pUnit:RegsiterEvent("TeleportDown_UpFront", 1000, 0)
 	pUnit:RegsiterEvent("TeleportDown_UpBack", 1000, 0)
 end
