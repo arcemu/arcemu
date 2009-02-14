3b24dd0793410cf1d61319e994a18126fd28f04e
Whoami <Whoami@arcemu.org>
2009-02-14 19:50:02 +0000

    Realm First achievements stay with the first character that achieved them (if that character gets deleted, the achievement can no longer be attained) http://arcemu.org/forums/index.php?showtopic=12709
    PCRE and ZLIB build in x64 with "all projects" solution
    http://arcemu.org/forums/index.php?showtopic=12737

--- src/arcemu-world/CharacterHandler.cpp
+++ src/arcemu-world/CharacterHandler.cpp
@@ -562,7 +562,7 @@ uint8 WorldSession::DeleteCharacter(uint32 guid)
 			CharacterDatabase.Execute("DELETE FROM mailbox WHERE player_guid = %u", (uint32)guid);
 			CharacterDatabase.Execute("DELETE FROM social_friends WHERE character_guid = %u OR friend_guid = %u", (uint32)guid, (uint32)guid);
 			CharacterDatabase.Execute("DELETE FROM social_ignores WHERE character_guid = %u OR ignore_guid = %u", (uint32)guid, (uint32)guid);
-			CharacterDatabase.Execute("DELETE FROM character_achievement WHERE guid = '%u'",(uint32)guid);
+			CharacterDatabase.Execute("DELETE FROM character_achievement WHERE guid = '%u' AND achievement NOT IN (457, 467, 466, 465, 464, 463, 462, 461, 460, 459, 458, 1404, 1405, 1406, 1407, 1408, 1409, 1410, 1411, 1412, 1413, 1415, 1414, 1416, 1417, 1418, 1419, 1420, 1421, 1422, 1423, 1424, 1425, 1426, 1427, 1463, 1400, 456, 1402)",(uint32)guid);
 			CharacterDatabase.Execute("DELETE FROM character_achievement_progress WHERE guid = '%u'",(uint32)guid);
 
 			/* remove player info */
