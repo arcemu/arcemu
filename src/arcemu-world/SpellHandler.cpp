7a21e4cce9503bfc149e4bcfb89fac304800d0b8
Slack <Slack@arcemu.org>
2009-03-23 22:09:40 +0000

    Fixed: item use bug.

--- src/arcemu-world/SpellHandler.cpp
+++ src/arcemu-world/SpellHandler.cpp
@@ -58,7 +58,7 @@ void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
 
 	if(sScriptMgr.CallScriptedItem(tmpItem,_player))
 		return;
-	
+
 	if( itemProto->InventoryType != 0 && !_player->GetItemInterface()->IsEquipped(itemProto->ItemId) )//Equipable items cannot be used before they're equipped. Prevents exploits
 		return;//Prevents exploits such as keeping an on-use trinket in your bag and using WPE to use it from your bag in mid-combat.
 
@@ -94,7 +94,7 @@ void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
 				}
 
 				// HookInterface
-				if (sHookInterface.OnCastSpell( _player, spellInfo ))
+				if (!sHookInterface.OnCastSpell( _player, spellInfo ))
 					return;
 
 				if (spellInfo->AuraInterruptFlags & AURA_INTERRUPT_ON_STAND_UP)
