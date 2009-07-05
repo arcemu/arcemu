65ea62c712cb3e63ca91fa9d69c1d6220e02de3d
SBAL <SBAL@arcemu.org>
2009-07-05 11:53:18 +0000

    CHANGED: Conditions in luaUnit_IsGM() to use predefined macro, by Sadikum, http://arcemu.org/forums/index.php?showtopic=16877

--- src/scripts/src/LUAScripting/LUAEngine.cpp
+++ src/scripts/src/LUAScripting/LUAEngine.cpp
@@ -4271,11 +4271,7 @@ int luaUnit_RemoveAurasType(lua_State * L, Unit * ptr)
 
 int luaUnit_IsGM( lua_State * L, Unit * ptr )
 {
-	if( ptr == NULL || !ptr->IsPlayer() )
-	{
-		lua_pushboolean( L, 0 );
-		return 0;
-	}
+	CHECK_TYPEID_RET( TYPEID_PLAYER );
 
 	Player * plr = objmgr.GetPlayer( ptr->GetLowGUID() );
 	if( plr == NULL )
