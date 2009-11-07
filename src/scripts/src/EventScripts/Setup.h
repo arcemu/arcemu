4d00c3cc436b19c58e425bd519c023421cd7d97d
Azolex <Azolex@arcemu.org>
2009-11-07 23:59:12 +0000

    * UPDATE: Scripts :)

--- src/scripts/src/EventScripts/Setup.h
+++
@@ -1,8 +0,0 @@
-#ifndef GOSSIP_SCRIPTS_SETUP_H
-#define GOSSIP_SCRIPTS_SETUP_H
-
-void SetupDarkmoonFaireGossip(ScriptMgr * mgr);
-void SetupDarkmoonFaireBarker(ScriptMgr * mgr);
-void SetupDarkmoonFaireObjects(ScriptMgr * mgr);
-
-#endif
