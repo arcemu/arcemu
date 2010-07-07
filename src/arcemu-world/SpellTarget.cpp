3cd3816cbdd58f7e00f2f227841a1395d25b51ac
WAmadeus <WAmadeus@arcemu.org>
2010-07-07 01:32:44 +0000

    FIXED: re-added Spell::SpellTargetEnemiesInAreaChanneled, but now it only fills m_targets coordinates, which were missing for Spell::SpellTargetAreaOfEffect

--- src/arcemu-world/SpellTarget.cpp
+++ src/arcemu-world/SpellTarget.cpp
@@ -99,8 +99,8 @@ pSpellTarget SpellTargetHandler[EFF_TARGET_LIST_LENGTH_MARKER] =
 	&Spell::SpellTargetSummon,				  // 73
 	&Spell::SpellTargetNULL,					// 74
 	&Spell::SpellTargetNULL,					// 75
-	&Spell::SpellTargetNULL,					// 76
-	&Spell::SpellTargetSingleTargetEnemy,	    // 77
+	&Spell::SpellTargetEnemiesInAreaChanneled,	// 76
+	&Spell::SpellTargetSingleTargetEnemy,		// 77
 	&Spell::SpellTargetNULL,					// 78
 	&Spell::SpellTargetNULL,					// 79
 	&Spell::SpellTargetNULL,					// 80
@@ -1263,4 +1263,18 @@ void Spell::SpellTargetNonCombatPet( uint32 i, uint32 j )
 		return; 
 	
 	SafeAddTarget( &m_targetUnits[i], u_caster->critterPet->GetGUID() );
+}
+
+void Spell::SpellTargetEnemiesInAreaChanneled(uint32 i, uint32 j)
+{
+	if( ! m_caster->IsInWorld() )
+		return;
+
+	Unit * target = m_caster->GetMapMgr()->GetUnit(m_targets.m_unitTarget);
+	if( target == NULL )
+		return;
+
+	m_targets.m_destX = target->GetPositionX();
+	m_targets.m_destY = target->GetPositionY();
+	m_targets.m_destZ = target->GetPositionZ();
 }
