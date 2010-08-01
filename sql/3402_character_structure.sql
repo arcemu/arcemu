6b4b413dab84bffdbcd87b31815cbcc7897ef21b
Paroxysm <Paroxysm@arcemu.org>
2010-08-01 00:00:05 +0000

    FIXED: syntax error thx FlickR

--- sql/3402_character_structure.sql
+++ sql/3402_character_structure.sql
@@ -811,10 +811,11 @@ CREATE TABLE `playerpetspells` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `pet_talents`;
-CREATE TABLE `pet_talents` (
+CREATE TABLE `pet_talents` 
+(
 	`pet_owner` INT(11) UNSIGNED NOT NULL,
 	`pet_family` TINYINT(3) NOT NULL,
-	`talent_array` VARCHAR(255),
+	`talent_array` VARCHAR(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Table structure for table `playersummons` */
