c11894bb6f7f3264a6114f60c76282ac1f31073f
dfighter1985 <dfighter1985.arcemu@gmail.com>
2010-11-05 03:29:37 +0000

    FIXED: Uh, it's not a bad idea to delete rows from the new tables too when deleting a character.

--- src/arcemu-world/CharacterHandler.cpp
+++ src/arcemu-world/CharacterHandler.cpp
@@ -555,6 +555,8 @@ uint8 WorldSession::DeleteCharacter(uint32 guid)
 			CharacterDatabase.Execute("DELETE FROM character_achievement WHERE guid = '%u' AND achievement NOT IN (457, 467, 466, 465, 464, 463, 462, 461, 460, 459, 458, 1404, 1405, 1406, 1407, 1408, 1409, 1410, 1411, 1412, 1413, 1415, 1414, 1416, 1417, 1418, 1419, 1420, 1421, 1422, 1423, 1424, 1425, 1426, 1427, 1463, 1400, 456, 1402)",(uint32)guid);
 			CharacterDatabase.Execute("DELETE FROM character_achievement_progress WHERE guid = '%u'",(uint32)guid);
 			CharacterDatabase.Execute("DELETE FROM playerspells WHERE GUID = '%u'", guid );
+			CharacterDatabase.Execute("DELETE FROM playerdeletedspells WHERE GUID = '%u'", guid );
+			CharacterDatabase.Execute("DELETE FROM playerskills WHERE GUID = '%u'", guid );
 
 			/* remove player info */
 			objmgr.DeletePlayerInfo((uint32)guid);
