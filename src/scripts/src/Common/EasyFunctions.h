2f79c03b22bb5a8f2ea8907366bf05558ce55f57
jackpoz <jackpoz.arcemu@gmail.com>
2010-11-21 13:26:35 +0000

    MODIFIED: Added linux socket files to arcemu-shared VS project (obviously excluded from build). MODIFIED: Added common scripts files to ArcScripts VS projects (they were included in the build but not in the project o.O). MODIFIED: Removed some awful NULLOBJ/ObjectPointer macros from EasyFunctions.

--- src/scripts/src/Common/EasyFunctions.h
+++ src/scripts/src/Common/EasyFunctions.h
@@ -34,28 +34,6 @@
 #define CREATE_GAMEOBJECT_SCRIPT(cl) &cl::Create
 #define CREATE_CREATURESCRIPT(cl) &cl::Create
 
-#define CreaturePointer				Creature *
-#define UnitPointer					Unit *
-#define PlayerPointer				Player *
-#define GameObjectPointer			GameObject *
-#define ObjectPointer				Object *
-#define SpellPointer				Spell *
-#define MapMgrPointer				MapMgr *
-#define PetPointer					Pet *
-#define AuraPointer					Aura *
-#define ItemPointer					Item *
-
-#define NULLOBJ						NULL
-#define NULLCREATURE				NULL
-#define NULLUNIT					NULL
-#define NULLGOB						NULL
-#define NULLPLR						NULL
-#define NULLCORPSE					NULL
-#define NULLSPELL					NULL
-#define NULLMAPMGR					NULL
-#define NULLAURA					NULL
-#define NULLITEM					NULL
-
 class SCRIPT_DECL EasyFunctions
 {
 public:
@@ -79,7 +57,7 @@ public:
 #endif
   }
   
-  void WaypointCreate(CreaturePointer pCreature , float x, float y, float z, float o, uint32 waittime, uint32 flags, uint32 modelid)
+  void WaypointCreate(Creature* pCreature , float x, float y, float z, float o, uint32 waittime, uint32 flags, uint32 modelid)
   {
     PrintMessage("Function call: WaypointCreate()");
     if(pCreature == NULL)
@@ -117,7 +95,7 @@ public:
 	
   }
 
-  void EnableWaypoints(CreaturePointer creat)
+  void EnableWaypoints(Creature* creat)
   {
 	  if(!creat)
 		  return;
@@ -127,7 +105,7 @@ public:
 	  creat->GetAIInterface()->SetWaypointMap(creat->m_custom_waypoint_map);
   }
   
-  void DeleteWaypoints(CreaturePointer creat)
+  void DeleteWaypoints(Creature* creat)
   {
     PrintMessage("Function call: DeleteWaypoints()");
     if(creat == NULL)
@@ -147,18 +125,18 @@ public:
     creat->m_custom_waypoint_map->clear();
   }
 
-  CreaturePointer SpawnCreature(PlayerPointer pThis, uint32 entry, float posX, float posY, float posZ, float posO, uint32 duration = 0, uint32 phase = 1)
+  Creature* SpawnCreature(Player* pThis, uint32 entry, float posX, float posY, float posZ, float posO, uint32 duration = 0, uint32 phase = 1)
   {
     PrintMessage("Function call: SpawnCreature()");
-    if(pThis == NULLPLR)
-		return NULLCREATURE;
+    if(pThis == NULL)
+		return NULL;
       
     CreatureProto *p = CreatureProtoStorage.LookupEntry(entry);
     
     if(p == NULL)
-      return NULLCREATURE;
+      return NULL;
 
-    CreaturePointer pCreature = pThis->GetMapMgr()->CreateCreature(entry);
+    Creature* pCreature = pThis->GetMapMgr()->CreateCreature(entry);
     pCreature->m_spawn = 0;
     pCreature->Load(p, posX, posY, posZ);
     pCreature->SetMapId(pThis->GetMapId());
@@ -171,7 +149,7 @@ public:
     return pCreature;
   }
   
-  void DeleteSpawned(CreaturePointer creat)
+  void DeleteSpawned(Creature* creat)
   {
     PrintMessage("Function call: DeleteSpawned()");
     if(creat == NULL)
@@ -180,21 +158,21 @@ public:
     creat->DeleteMe();
   }
 
-  CreaturePointer SpawnCreatureExtended(PlayerPointer pThis, uint32 entry, float posX, 
+  Creature* SpawnCreatureExtended(Player* pThis, uint32 entry, float posX, 
                                          float posY, float posZ, float posO, uint32 faction, 
                                          uint32 duration, bool dis_comb = false, bool dis_mel = false, 
                                          bool dis_target = false)
   {
     PrintMessage("Function call: SpawnCreatureExtended()");
 
-    if(pThis == NULLPLR)
-      return NULLCREATURE;
+    if(pThis == NULL)
+      return NULL;
 
     CreatureProto *p = CreatureProtoStorage.LookupEntry(entry);
 	if(p == NULL)
-		return NULLCREATURE;
+		return NULL;
 
-    CreaturePointer pCreature  = pThis->GetMapMgr()->CreateCreature(entry);
+    Creature* pCreature  = pThis->GetMapMgr()->CreateCreature(entry);
     pCreature->m_spawn = 0;
     pCreature->Load(p, posX, posY, posZ);
     pCreature->SetMapId(pThis->GetMapId());
@@ -211,7 +189,7 @@ public:
     return pCreature;
   }
   
-  void GameobjectDelete(GameObjectPointer pC, uint32 duration)
+  void GameobjectDelete(GameObject* pC, uint32 duration)
   {
     PrintMessage("Function call: GameobjectDelete()");
     if(pC == NULL)
@@ -220,17 +198,17 @@ public:
 	pC->Despawn(duration,0);
   }
 
-  GameObjectPointer SpawnGameobject(PlayerPointer plr, uint32 entry_id, float x, float y, float z, float o, float scale, float orientation1, float orientation2, float orientation3, float orientation4 )
+  GameObject* SpawnGameobject(Player* plr, uint32 entry_id, float x, float y, float z, float o, float scale, float orientation1, float orientation2, float orientation3, float orientation4 )
   {
     PrintMessage("Function call: SpawnGameobject()");
-	if(plr == NULLPLR)
-		return NULLGOB;
+	if(plr == NULL)
+		return NULL;
 
     GameObjectInfo *goi = GameObjectNameStorage.LookupEntry(entry_id);
 	if(goi == NULL)
-		return NULLGOB;
+		return NULL;
 
-    GameObjectPointer pC = plr->GetMapMgr()->CreateGameObject(entry_id);
+    GameObject* pC = plr->GetMapMgr()->CreateGameObject(entry_id);
     //pC->spawnid=0;
     pC->m_spawn=0;
     pC->CreateFromProto(entry_id, plr->GetMapId(), (float)x, (float)y, (float)z, (float)o, float(orientation1), float(orientation2), float(orientation3), float(orientation4));
@@ -242,7 +220,7 @@ public:
     return pC;
   }
   
-  void MoveToPlayer(PlayerPointer plr, CreaturePointer creat)
+  void MoveToPlayer(Player* plr, Creature* creat)
   {
     PrintMessage("Function call: MoveToPlayer()");
     if(plr == NULL)
@@ -254,7 +232,7 @@ public:
     creat->GetAIInterface()->MoveTo(plr->GetPositionX(), plr->GetPositionY(), plr->GetPositionZ(), plr->GetOrientation());
   }
 
-  void CreateCustomWaypointMap(CreaturePointer creat)
+  void CreateCustomWaypointMap(Creature* creat)
   {
     PrintMessage("Function call: CreateCustomWaypointMap()");
     if(creat == NULL)
@@ -266,14 +244,14 @@ public:
     creat->GetAIInterface()->SetWaypointMap(creat->m_custom_waypoint_map);
   }
   
-	bool AddItem( uint32 pEntry, PlayerPointer pPlayer, uint32 pCount = 1 )
+	bool AddItem( uint32 pEntry, Player* pPlayer, uint32 pCount = 1 )
 	{
 		PrintMessage( "Function call: AddItem( %u )", pEntry );
-		if ( pPlayer == NULLPLR || pEntry == 0 || pCount == 0 )
+		if ( pPlayer == NULL || pEntry == 0 || pCount == 0 )
 			return false;
 
-		ItemPointer ItemStack = pPlayer->GetItemInterface()->FindItemLessMax( pEntry, pCount, false );
-		if ( ItemStack == NULLITEM )
+		Item* ItemStack = pPlayer->GetItemInterface()->FindItemLessMax( pEntry, pCount, false );
+		if ( ItemStack == NULL )
 		{
 			ItemPrototype* ItemProto = ItemPrototypeStorage.LookupEntry( pEntry );
 			if ( ItemProto == NULL )
@@ -282,13 +260,13 @@ public:
 			SlotResult Result = pPlayer->GetItemInterface()->FindFreeInventorySlot( ItemProto );
 			if ( !Result.Result )
 			{
-				pPlayer->GetItemInterface()->BuildInventoryChangeError( NULLITEM, NULLITEM, INV_ERR_INVENTORY_FULL );
+				pPlayer->GetItemInterface()->BuildInventoryChangeError( NULL, NULL, INV_ERR_INVENTORY_FULL );
 				return false;
 			}
 			else
 			{
-				ItemPointer NewItem = objmgr.CreateItem( pEntry, pPlayer );
-				if ( NewItem == NULLITEM )
+				Item* NewItem = objmgr.CreateItem( pEntry, pPlayer );
+				if ( NewItem == NULL )
 					return false;
 
 				if ( ItemProto->MaxCount < pCount )
@@ -297,7 +275,7 @@ public:
 				NewItem->SetStackCount( pCount );
 				if ( pPlayer->GetItemInterface()->SafeAddItem( NewItem, Result.ContainerSlot, Result.Slot ) == ADD_ITEM_RESULT_ERROR )
 				{
-					NewItem = NULLITEM;
+					NewItem = NULL;
 					return false;
 				};
 
@@ -318,42 +296,42 @@ public:
 		return false;
 	};
   
-  void EventCreatureDelete(CreaturePointer creat, uint32 time)  // Creature and time in ms
+  void EventCreatureDelete(Creature* creat, uint32 time)  // Creature and time in ms
   {
 	  sEventMgr.AddEvent(creat, &Creature::DeleteMe, EVENT_CREATURE_SAFE_DELETE, time, 1, EVENT_FLAG_DO_NOT_EXECUTE_IN_WORLD_CONTEXT);
   }
 
-  void EventCastSpell(UnitPointer caster, UnitPointer target, uint32 spellid, uint32 time)
+  void EventCastSpell(Unit* caster, Unit* target, uint32 spellid, uint32 time)
   {
 	  sEventMgr.AddEvent(TO_UNIT(caster), &Unit::EventCastSpell, TO_UNIT(target), dbcSpell.LookupEntry(spellid), EVENT_UNK, time, 0, EVENT_FLAG_DO_NOT_EXECUTE_IN_WORLD_CONTEXT);
   }
 
-  void EventPlaySound(CreaturePointer creat, uint32 id, uint32 time)
+  void EventPlaySound(Creature* creat, uint32 id, uint32 time)
   {
 	  sEventMgr.AddEvent(TO_OBJECT(creat), &Object::PlaySoundToSet, id, EVENT_UNK, time, 1, EVENT_FLAG_DO_NOT_EXECUTE_IN_WORLD_CONTEXT);
   }
 
-  void EventCreatureSay(CreaturePointer creat, string say,  uint32 time)
+  void EventCreatureSay(Creature* creat, string say,  uint32 time)
   {
 	  creat->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, say.c_str(), time);
   }
 
-  void EventCreatureYell(CreaturePointer creat, string say,  uint32 time)
+  void EventCreatureYell(Creature* creat, string say,  uint32 time)
   {
 	  creat->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, say.c_str(), time);
   }
 
-  CreaturePointer getNpcQuester(PlayerPointer plr, uint32 npcid)
+  Creature* getNpcQuester(Player* plr, uint32 npcid)
   {
 	  if( plr == NULL || plr->GetMapMgr() == NULL || plr->GetMapMgr()->GetInterface() == NULL )
-		  return NULLCREATURE;
+		  return NULL;
 	  return plr->GetMapMgr()->GetInterface()->GetCreatureNearestCoords(plr->GetPositionX(), plr->GetPositionY(), plr->GetPositionZ(), npcid);
   }
 
-  GameObjectPointer getGOQuester(PlayerPointer plr, uint32 goid)
+  GameObject* getGOQuester(Player* plr, uint32 goid)
   {
 	  if( plr == NULL || plr->GetMapMgr() == NULL || plr->GetMapMgr()->GetInterface() == NULL )
-		  return NULLGOB;
+		  return NULL;
 	  return plr->GetMapMgr()->GetInterface()->GetGameObjectNearestCoords(plr->GetPositionX(), plr->GetPositionY(), plr->GetPositionZ(), goid);
   }
 
