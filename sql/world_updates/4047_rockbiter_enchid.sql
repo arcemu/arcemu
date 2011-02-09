87a2a9f7debd94eec9ba18da6c7a5556209dc9a9
dfighter1985 <dfighter1985.arcemu@gmail.com>
2011-02-09 19:34:35 +0000

    FIXED: Rockbiter weapon shaman spells' enchantment ID. Fixes ticket #70.

---
+++ sql/world_updates/4047_rockbiter_enchid.sql
@@ -0,0 +1,4 @@
+UPDATE `spell_effects_override` SET `MiscValue` = '3023' WHERE `spellId` = '8017' AND `EffectID` = '1';
+UPDATE `spell_effects_override` SET `MiscValue` = '3024' WHERE `spellId` = '8018' AND `EffectID` = '1';
+UPDATE `spell_effects_override` SET `MiscValue` = '1' WHERE `spellId` = '8019' AND `EffectID` = '1';
+UPDATE `spell_effects_override` SET `MiscValue` = '3032' WHERE `spellId` = '10399' AND `EffectID` = '1';
