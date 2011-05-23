e220c4a7dd59a5dbbd3e0da4be5af028bbaeb186
dfighter1985 <dfighter1985.arcemu@gmail.com>
2011-05-23 17:39:40 +0000

    FIXED: It's not a bad idea to be able to find mysql lib on FreeBSD too :P

--- cmake_modules/FindMySQL.cmake
+++ cmake_modules/FindMySQL.cmake
@@ -20,7 +20,7 @@ FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
 SET(MYSQL_NAMES mysqlclient mysqlclient_r)
 FIND_LIBRARY(MYSQL_LIBRARY
   NAMES ${MYSQL_NAMES}
-  PATHS /usr/lib /usr/local/lib /usr/local/mysql/lib
+  PATHS /usr/lib /usr/local/lib /usr/local/mysql/lib /usr/local/lib/mysql
 )
 
 IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
