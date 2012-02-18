3be2ca073aaa74ce3493a29e207a9b4c03a7d121
Neo_mat <neomat.github@gmail.com>
2012-02-19 00:34:39 +0400

    Auras with interrupt flags AURA_INTERRUPT_ON_JUMP, AURA_INTERRUPT_ON_TURNING and AURA_INTERRUPT_ON_CAST_SPELL are being removed now when such actions are done. Fixes Hunter's Feign Death, now you can't jump, turn your "dead" body and cast any spell you want while under effect of Feign Death. Closses #71.

--- src/arcemu-world/Spell.h
+++ src/arcemu-world/Spell.h
@@ -321,33 +321,33 @@ enum CastInterruptFlags
 
 enum AuraInterruptFlags
 {
-    AURA_INTERRUPT_NULL							= 0x0,
-    AURA_INTERRUPT_ON_HOSTILE_SPELL_INFLICTED	= 0x1,
-    AURA_INTERRUPT_ON_ANY_DAMAGE_TAKEN			= 0x2,
-    AURA_INTERRUPT_ON_UNK1						= 0x4,
-    AURA_INTERRUPT_ON_MOVEMENT					= 0x8,
-    AURA_INTERRUPT_ON_TURNING					= 0x10,
-    AURA_INTERRUPT_ON_ENTER_COMBAT				= 0x20,
-    AURA_INTERRUPT_ON_DISMOUNT					= 0x40,
-    AURA_INTERRUPT_ON_ENTER_WATER				= 0x80,
-    AURA_INTERRUPT_ON_LEAVE_WATER				= 0x100, // could be AURA_INTERRUPT_ON_LEAVE_CURRENT_SURFACE
-    AURA_INTERRUPT_ON_UNUSED2					= 0x200,
-    AURA_INTERRUPT_ON_UNK4						= 0x400,
-    AURA_INTERRUPT_ON_UNK5						= 0x800,
-    AURA_INTERRUPT_ON_START_ATTACK				= 0x1000,
-    AURA_INTERRUPT_ON_UNK6						= 0x2000,
-    AURA_INTERRUPT_ON_UNUSED3					= 0x4000,
-    AURA_INTERRUPT_ON_CAST_SPELL				= 0x8000,
-    AURA_INTERRUPT_ON_UNK7						= 0x10000,
-    AURA_INTERRUPT_ON_MOUNT						= 0x20000,
-    AURA_INTERRUPT_ON_STAND_UP					= 0x40000,
-    AURA_INTERRUPT_ON_LEAVE_AREA				= 0x80000,
-    AURA_INTERRUPT_ON_INVINCIBLE				= 0x100000,
-    AURA_INTERRUPT_ON_STEALTH					= 0x200000,
-    AURA_INTERRUPT_ON_UNK8						= 0x400000,
-    AURA_INTERRUPT_ON_PVP_ENTER					= 0x800000,
-    AURA_INTERRUPT_ON_DIRECT_DAMAGE				= 0x1000000,
-    AURA_INTERRUPT_ON_AFTER_CAST_SPELL			= 0x80000000,
+    AURA_INTERRUPT_NULL 						= 0x00000000,
+    AURA_INTERRUPT_ON_HOSTILE_SPELL_INFLICTED	= 0x00000001, // Remove on being hit by hostile spell
+    AURA_INTERRUPT_ON_ANY_DAMAGE_TAKEN			= 0x00000002, // Remove on any damage taken
+    AURA_INTERRUPT_ON_CAST						= 0x00000004, // Remove on any spell cast
+    AURA_INTERRUPT_ON_MOVEMENT					= 0x00000008, // Remove on moving
+    AURA_INTERRUPT_ON_TURNING					= 0x00000010, // Remove on turning
+    AURA_INTERRUPT_ON_JUMP						= 0x00000020, // Remove on jumping
+    AURA_INTERRUPT_ON_DISMOUNT					= 0x00000040, // Remove on dismounting
+    AURA_INTERRUPT_ON_ENTER_WATER				= 0x00000080, // Remove when entering water
+    AURA_INTERRUPT_ON_LEAVE_WATER				= 0x00000100, // Remove when leaving water, could be AURA_INTERRUPT_ON_LEAVE_CURRENT_SURFACE
+    AURA_INTERRUPT_ON_UNUSED2					= 0x00000200, // Something to do with sheath state
+    AURA_INTERRUPT_ON_UNK4						= 0x00000400, // On gossip NPC?
+    AURA_INTERRUPT_ON_UNK5						= 0x00000800, // ??
+    AURA_INTERRUPT_ON_START_ATTACK				= 0x00001000, // Remove on melee attack
+    AURA_INTERRUPT_ON_CAST_SPELL				= 0x00002000, // Remove on spell cast
+    AURA_INTERRUPT_ON_UNUSED3					= 0x00004000,
+    AURA_INTERRUPT_ON_CAST_SPELL_UNK			= 0x00008000, // Not sure what this one is, but definitely not on spell cast
+    AURA_INTERRUPT_ON_UNK7						= 0x00010000,
+    AURA_INTERRUPT_ON_MOUNT 					= 0x00020000, // Remove on mounting up
+    AURA_INTERRUPT_ON_STAND_UP					= 0x00040000, // Remove on stand up, for spells like drinking/eating
+    AURA_INTERRUPT_ON_LEAVE_AREA				= 0x00080000, // Remove on leaving zone or being teleported
+    AURA_INTERRUPT_ON_INVINCIBLE				= 0x00100000, // Remove on becoming invincible
+    AURA_INTERRUPT_ON_STEALTH					= 0x00200000, // Remove on entering stealth
+    AURA_INTERRUPT_ON_UNK8						= 0x00400000,
+    AURA_INTERRUPT_ON_PVP_ENTER 				= 0x00800000, // Remove on getting PvP flag
+    AURA_INTERRUPT_ON_DIRECT_DAMAGE 			= 0x01000000, // Remove on taking direct damage
+    AURA_INTERRUPT_ON_AFTER_CAST_SPELL			= 0x80000000, // Custom?
 };
 
 enum ChannelInterruptFlags
