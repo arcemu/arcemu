f6126a5f156be9de970c143ba16ddf8ff17bb4db
sanctum32 <andriuspel@gmail.com>
2014-06-06 10:58:35 +0300

    Fixed sql typo

--- sql/world_updates/2014-06-05_01-15_worldstring.sql
+++ sql/world_updates/2014-06-05_01-15_worldstring.sql
@@ -2,4 +2,4 @@ DELETE FROM `worldstring_tables` WHERE `entry` between 85 and 88;
 INSERT INTO `worldstring_tables` (`text`) VALUES 
 ('Trick or Treat!'),
 ('You must be at least level %u to buy Arena charter');
-UPDATE `world_db_version` SET `LastUpdate` = '2014-06-05_00-47_worldstring' WHERE `LastUpdate` = '2014-06-05_01-15_worldstring';
+UPDATE `world_db_version` SET `LastUpdate` = '2014-06-05_01-15_worldstring' WHERE `LastUpdate` = '2014-06-05_00-47_worldstring';
