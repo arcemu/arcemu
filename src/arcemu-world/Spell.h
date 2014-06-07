e86e323007187899f752a9b9af5d3509b6f5241f
sanctum32 <andriuspel@gmail.com>
2014-06-07 12:34:50 +0300

    Code cleanup

--- src/arcemu-world/Spell.h
+++ src/arcemu-world/Spell.h
@@ -1400,6 +1400,26 @@ typedef enum
     EFF_TARGET_LIST_LENGTH_MARKER						= 111,
 } SpellEffectTarget;
 
+// uint16 0xFFFF
+enum SpellStartFlags
+{
+    //0x01
+    SPELL_START_FLAG_DEFAULT = 0x02, // atm set as default flag
+    //0x04
+    //0x08
+    //0x10
+    SPELL_START_FLAG_RANGED = 0x20,
+    //0x40
+    //0x80
+    //0x100
+    //0x200
+    //0x400
+    //0x800
+    //0x1000
+    //0x2000
+    //0x4000
+    //0x8000
+};
 
 inline bool HasTargetType(SpellEntry* sp, uint32 ttype)
 {
