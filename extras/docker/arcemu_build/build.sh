3ee41c1bd0e70555cbc55db6212a06f5144d031b
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2018-12-09 17:33:07 +0100

    Oups, I messed this one up

--- extras/docker/arcemu_build/build.sh
+++ extras/docker/arcemu_build/build.sh
@@ -1,19 +1,3 @@
-FROM ubuntu:xenial
-
-RUN apt-get update
-RUN apt-get install -y git cmake make g++
-RUN apt-get install -y zlib1g-dev libssl-dev libpcre3-dev libbz2-dev libmysqlclient-dev libmysql++-dev
-
-RUN mkdir -p /srv/build/arcemu
-RUN mkdir -p /srv/build/arcemu/bin
-COPY build.sh /srv/build/arcemu/build.sh
-RUN chmod a+x /srv/build/arcemu/build.sh
-
-WORKDIR /srv/build/arcemu
-CMD ["./build.sh"]
-
-root@ubuntu-xenial:~/docker# ^C
-root@ubuntu-xenial:~/docker# cat build.sh
 #!/bin/bash
 
 git clone https://github.com/arcemu/arcemu.git src
