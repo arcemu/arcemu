6068f891450a729991e353667f9372683e79513a
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2020-06-13 20:20:57 +0200

    Moved these to their proper files

--- src/world/Level3.cpp
+++ src/world/Level3.cpp
@@ -4450,3 +4450,70 @@ bool ChatHandler::HandleNPCCastCommand(const char* args, WorldSession* m_session
 
 	return true;
 }
+
+bool ChatHandler::HandleAddKillCommand(const char* args, WorldSession* m_session)
+{
+	uint32 KillAmount = args ? atol(args) : 1;
+	Player* plr = getSelectedChar(m_session, true);
+	if(plr == NULL)
+		return true;
+
+	BlueSystemMessage(m_session, "Adding %u kills to player %s.", KillAmount, plr->GetName());
+	GreenSystemMessage(plr->GetSession(), "You have had %u honor kills added to your character.", KillAmount);
+
+	plr->m_killsToday += KillAmount;
+	plr->m_killsLifetime += KillAmount;
+	plr->SetUInt32Value(PLAYER_FIELD_KILLS, uint16(plr->m_killsToday) | (plr->m_killsYesterday << 16));
+	plr->SetUInt32Value(PLAYER_FIELD_LIFETIME_HONORBALE_KILLS, plr->m_killsLifetime);
+
+	return true;
+}
+
+bool ChatHandler::HandleAddHonorCommand(const char* args, WorldSession* m_session)
+{
+	uint32 HonorAmount = args ? atol(args) : 1;
+	Player* plr = getSelectedChar(m_session, true);
+	if(plr == 0)
+		return true;
+
+	BlueSystemMessage(m_session, "Adding %u honor to player %s.", HonorAmount, plr->GetName());
+	GreenSystemMessage(plr->GetSession(), "You have had %u honor points added to your character.", HonorAmount);
+
+	HonorHandler::AddHonorPointsToPlayer(plr, HonorAmount);
+	return true;
+}
+
+bool ChatHandler::HandlePVPCreditCommand(const char* args, WorldSession* m_session)
+{
+	uint32 Rank, Points;
+	if(sscanf(args, "%u %u", (unsigned int*)&Rank, (unsigned int*)&Points) != 2)
+	{
+		RedSystemMessage(m_session, "Command must be in format <rank> <points>.");
+		return true;
+	}
+	Points *= 10;
+	uint64 Guid = m_session->GetPlayer()->GetSelection();
+	if(Guid == 0)
+	{
+		RedSystemMessage(m_session, "A selection of a unit or player is required.");
+		return true;
+	}
+
+	BlueSystemMessage(m_session, "Building packet with Rank %u, Points %u, GUID " I64FMT ".",
+	                  Rank, Points, Guid);
+
+	WorldPacket data(SMSG_PVP_CREDIT, 12);
+	data << Points << Guid << Rank;
+	m_session->SendPacket(&data);
+	return true;
+}
+
+bool ChatHandler::HandleGlobalHonorDailyMaintenanceCommand(const char* args, WorldSession* m_session)
+{
+	return false;
+}
+
+bool ChatHandler::HandleNextDayCommand(const char* args, WorldSession* m_session)
+{
+	return false;
+}
--- src/world/MiscHandler.cpp
+++ src/world/MiscHandler.cpp
@@ -2557,3 +2557,16 @@ void WorldSession::HandleRealmSplitOpcode(WorldPacket & recv_data)
 	data << split_date;
 	SendPacket(&data);
 }
+
+void WorldSession::HandleSetVisibleRankOpcode(WorldPacket & recv_data)
+{
+	CHECK_INWORLD_RETURN
+
+	CHECK_PACKET_SIZE(recv_data, 4);
+	uint32 ChosenRank;
+	recv_data >> ChosenRank;
+	if(ChosenRank == 0xFFFFFFFF)
+		_player->SetChosenTitle(0);
+	else if(_player->HasTitle(static_cast< RankTitles >(ChosenRank)))
+		_player->SetChosenTitle(ChosenRank);
+}
