039c515670560342663ed1a6d44e92189a2297d7
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2020-06-14 12:23:53 +0200

    Tell the client that the quest is repeatable in the quest list packet. Thanks to zdroid9770 for the PR. Closes #392

--- src/world/QuestMgr.cpp
+++ src/world/QuestMgr.cpp
@@ -662,7 +662,7 @@ void QuestMgr::BuildQuestList(WorldPacket* data, Object* qst_giver, Player* plr,
 				}
 				*data << int32((*it)->qst->questlevel);
 				*data << uint32((*it)->qst->quest_flags);
-				*data << uint8(0);   // According to MANGOS: "changes icon: blue question or yellow exclamation"
+				*data << uint8( (*it)->qst->is_repeatable );
 
 				if(lq)
 					*data << lq->Title;
