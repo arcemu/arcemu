794f133c03510b1d2564771e390efc3276a43a0d
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2023-04-30 19:50:46 +0200

    Scuttling mechano-tank vehicle-id and spells

---
+++ sql/world_updates/20230430-1949_scuttling_mechano-tank.sql
@@ -0,0 +1,4 @@
+UPDATE creature_proto SET vehicleid = 745 WHERE entry = 39713;
+UPDATE creature_proto SET spell1 = 74157 WHERE entry = 39713;
+UPDATE creature_proto SET spell2 = 74159 WHERE entry = 39713;
+UPDATE creature_proto SET spell3 = 74160 WHERE entry = 39713;
