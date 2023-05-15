8677c0b3783ab117a523cd461d7e25bd38d44445
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2023-05-15 02:01:13 +0200

    Implemented Object.setUInt64Value() method in Arcemu Python Engine

--- src/plugins/PythonEngine/python/modules/ArcPyObject.cpp
+++ src/plugins/PythonEngine/python/modules/ArcPyObject.cpp
@@ -300,7 +300,7 @@ static PyObject* ArcPyObject_getMapMgr( ArcPyObject *self, PyObject *args )
 ///   None
 ///
 /// Example
-///   mapMgr = object.getMapMgr
+///   object.setUInt32Value( 123, 0 )
 ///
 static PyObject* ArcPyObject_setUInt32Value( ArcPyObject *self, PyObject *args )
 {
@@ -320,6 +320,37 @@ static PyObject* ArcPyObject_setUInt32Value( ArcPyObject *self, PyObject *args )
 	Py_RETURN_NONE;
 }
 
+/// setUInt64Value
+///   Sets an uint64 field's value
+///
+/// Parameters
+///   index    -   The field's index
+///   value    -   The value that should be set
+///
+/// Return value
+///   None
+///
+/// Example
+///   object.setUInt64Value( 123, 0 )
+///
+static PyObject* ArcPyObject_setUInt64Value( ArcPyObject *self, PyObject *args )
+{
+	uint32 index;
+	uint64 value;
+
+	if( !PyArg_ParseTuple( args, "kK", &index, &value ) )
+	{
+		PyErr_SetString( PyExc_ValueError, "This method requires an index, and a value parameter" );
+		return NULL;
+	}
+
+	Object *obj = self->objectPtr;
+
+	obj->SetUInt64Value( index, value );
+
+	Py_RETURN_NONE;
+}
+
 static PyMethodDef ArcPyObject_methods[] = 
 {
 	{ "getPositionX", (PyCFunction)ArcPyObject_getPositionX, METH_VARARGS, "Returns the X coordinate of the Object" },
@@ -332,6 +363,7 @@ static PyMethodDef ArcPyObject_methods[] =
 	{ "getObjectsInRange", (PyCFunction)ArcPyObject_getObjectsInRange, METH_NOARGS, "Returns the objects in range of this object" },
 	{ "getMapMgr", (PyCFunction)ArcPyObject_getMapMgr, METH_NOARGS, "Returns the MapMgr of the Object" },
 	{ "setUInt32Value", (PyCFunction)ArcPyObject_setUInt32Value, METH_VARARGS, "Sets the value of an UInt32 field" },
+	{ "setUInt64Value", (PyCFunction)ArcPyObject_setUInt64Value, METH_VARARGS, "Sets the value of an UInt64 field" },
 	{NULL}
 };
 
