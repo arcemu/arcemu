5ae7921ed15bce40abb24a4e40308bca81f20cb3
cressidagp <gcpigni@gmail.com>
2023-06-16 17:03:52 -0300

    Actually, i also forget to remove the emote from quest plugin.

--- src/plugins/QuestScripts/QuestHooks.cpp
+++ src/plugins/QuestScripts/QuestHooks.cpp
@@ -83,7 +83,6 @@ void ReportToGoldshire(Player* pPlayer, Object* pObject)
 	char msg[256];
 	snprintf((char*)msg, 256, "You are dismissed %s . ", pPlayer->GetName());
 	(TO_CREATURE(pObject))->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, msg);
-	(TO_CREATURE(pObject))->Emote(EMOTE_ONESHOT_SALUTE_NOSHEATH);
 }
 
 void ZuluhedtheWhacked(Player* pPlayer, Object* pObject)
