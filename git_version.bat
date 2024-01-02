a30b905be63e3259c76dd18e5df3b2b2a26cca84
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2024-01-02 03:59:29 +0100

    Don't echo the comment when running the git version extractor on Windows

--- git_version.bat
+++ git_version.bat
@@ -1,3 +1,3 @@
-REM sadly CMake cannot directly execute the shell scripts under msysgit even if they are associated
 @echo off
+REM sadly CMake cannot directly execute the shell scripts under msysgit even if they are associated
 start git_version.sh
