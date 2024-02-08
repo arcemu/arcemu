bd327df3151c905bdb866dcb7b87e4b9f3271a0a
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2024-02-08 22:59:33 +0100

    Added test cases for Math::calcAngle

--- src/world/Tests/MathTest.cpp
+++ src/world/Tests/MathTest.cpp
@@ -25,6 +25,15 @@
 class MathTest
 {
 public:
+	bool testCalcAngle0();
+	bool testCalcAngle45();
+	bool testCalcAngle90();
+	bool testCalcAngle135();
+	bool testCalcAngle180();
+	bool testCalcAngle225();
+	bool testCalcAngle270();
+	bool testCalcAngle315();
+
 	bool testDiffWhenALarger();
 	bool testDiffWhenBLarger();
 	bool testDiffWhenAEqualsB();
@@ -37,6 +46,15 @@ public:
 };
 
 TESTCASE_REGISTRY_FOR( MathTest )
+	TESTCASE( MathTest, testCalcAngle0,       "Test that calcAngle returns 0.0 " )
+	TESTCASE( MathTest, testCalcAngle45,      "Test that calcAngle returns 45.0 " )
+	TESTCASE( MathTest, testCalcAngle90,      "Test that calcAngle returns 90.0 " )
+	TESTCASE( MathTest, testCalcAngle135,     "Test that calcAngle returns 135.0 " )
+	TESTCASE( MathTest, testCalcAngle180,     "Test that calcAngle returns 180.0 " )
+	TESTCASE( MathTest, testCalcAngle225,     "Test that calcAngle returns 225.0 " )
+	TESTCASE( MathTest, testCalcAngle270,     "Test that calcAngle returns 270.0 " )
+	TESTCASE( MathTest, testCalcAngle315,     "Test that calcAngle returns 315.0 " )
+
 	TESTCASE( MathTest, testDiffWhenALarger,  "Test that the diff method works when A is the larger number" )
 	TESTCASE( MathTest, testDiffWhenBLarger,  "Test that the diff method works when B is the larger number" )
 	TESTCASE( MathTest, testDiffWhenAEqualsB, "Test that the diff method works when A equals B" )
@@ -84,6 +102,63 @@ bool MathTest::testClampWhenLarger()
 bool MathTest::testClampWhenInRange()
 {
 	TEST_EQ( 60, Math::clamp< uint32 >( 60, 50, 100 ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle0()
+{
+	TEST_EQ( 0.0f, Math::calcAngle( 10.0f, 10.0f, 10.0f, 10.0f ) );
+	TEST_EQ( 0.0f, Math::calcAngle( 10.0f, 10.0f, 20.0f, 10.0f ) );	
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle45()
+{
+	TEST_EQ( 45.0f, Math::calcAngle( 10.0f, 10.0f, 20.0f, 20.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle90()
+{
+	TEST_EQ( 90.0f, Math::calcAngle( 10.0f, 10.0f, 10.0f, 20.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle135()
+{
+	TEST_EQ( 135.0f, Math::calcAngle( 10.0f, 10.0f, 0.0f, 20.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle180()
+{
+	TEST_EQ( 180.0f, Math::calcAngle( 10.0f, 10.0f, 0.0f, 10.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle225()
+{
+	TEST_EQ( 225.0f, Math::calcAngle( 10.0f, 10.0f, 0.0f, 0.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle270()
+{
+	TEST_EQ( 270.0f, Math::calcAngle( 10.0f, 10.0f, 10.0f, 0.0f ) );
+
+	TESTCASE_END();
+}
+
+bool MathTest::testCalcAngle315()
+{
+	TEST_EQ( 315.0f, Math::calcAngle( 10.0f, 10.0f, 20.0f, 0.0f ) );
 
 	TESTCASE_END();
 }
